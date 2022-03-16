dbdc9cbabc1b2a126efc05e16b53563e02c66990
Daryna Strakhar <strakhar.daryna@gmail.com>
2022-03-16 18:27:05 +0300

    блалал

--- game1/app/src/main/java/com/example/game1/Level1_2.java
+++ game1/app/src/main/java/com/example/game1/Level1_2.java
@@ -1,12 +1,14 @@
 package com.example.game1;
 
 import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -20,10 +22,38 @@ public class Level1_2 extends AppCompatActivity {
         w.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         String str = "";
         StringBuilder guessWord = new StringBuilder(str);
+
         Button letter1 = findViewById(R.id.letter1);
         Button letter2 = findViewById(R.id.letter2);
         Button letter3 = findViewById(R.id.letter3);
         Integer xxx = 0;
+
+        ImageView btnGuessWord = findViewById(R.id.btnGuessWord);
+
+        TextView ntg_1 = findViewById(R.id.ntg_1);
+        TextView ntg_2 = findViewById(R.id.ntg_2);
+        TextView[] ntg_words = {ntg_1, ntg_2};
+
+        btnGuessWord.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                boolean isFound = false;
+                for (int i = 0; i < 2; i++) {
+                    StringBuilder ngt = (StringBuilder) ntg_words[i].getText();
+                    if (ngt == guessWord){
+                        isFound = true;
+                        ntg_words = ArrayUtils.remove(ntg_words, i);
+                }
+                    if (isFound = true){
+                        textView.setTextColor(0x265E4A);
+
+                    }
+                    
+                }
+
+            }
+        });
+
 
         letter1.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -35,7 +65,10 @@ public class Level1_2 extends AppCompatActivity {
                     guessWord.append(character);
                 }
             }
+
         });
+
+
 
     }
     // системная кнопка назад
