eeed766546e528074703a9b2b9b35ead1ee50e15
Daryna Strakhar <strakhar.daryna@gmail.com>
2022-03-16 11:49:23 +0300

    что-то наворотила

--- game1/app/src/main/java/com/example/game1/Level1_2.java
+++ game1/app/src/main/java/com/example/game1/Level1_2.java
@@ -2,8 +2,11 @@ package com.example.game1;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -15,6 +18,24 @@ public class Level1_2 extends AppCompatActivity {
         setContentView(R.layout.level1_2);
         Window w = getWindow();
         w.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        String str = "";
+        StringBuilder guessWord = new StringBuilder(str);
+        Button letter1 = findViewById(R.id.letter1);
+        Button letter2 = findViewById(R.id.letter2);
+        Button letter3 = findViewById(R.id.letter3);
+        Integer xxx = 0;
+
+        letter1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (xxx == 0 || xxx == 2 || xxx == 4)
+                {
+                    letter1.setBackgroundColor(0x3BEECE);
+                    CharSequence character = letter1.getText();
+                    guessWord.append(character);
+                }
+            }
+        });
 
     }
     // системная кнопка назад
