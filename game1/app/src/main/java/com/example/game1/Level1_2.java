089658b3a43934084294ea313ffa37a80b8d25f6
Daryna Strakhar <strakhar.daryna@gmail.com>
2022-03-26 09:17:21 +0300

    ецуун5

--- game1/app/src/main/java/com/example/game1/Level1_2.java
+++ game1/app/src/main/java/com/example/game1/Level1_2.java
@@ -276,7 +276,9 @@ public class Level1_2 extends AppCompatActivity {
                 }
 
 
-                /* если уровень пройден */
+                if (levelIsPassed[0]){
+                    popUpEnd();
+                }
 
             }
 
