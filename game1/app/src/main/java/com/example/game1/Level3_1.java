1a335e88e21cc985d5694309c984c3a4d6332b6b
IraTheBestInTheWorld <i.romashkoo@gmail.com>
2022-04-05 13:08:49 +0300

    3 level

---
+++ game1/app/src/main/java/com/example/game1/Level3_1.java
@@ -0,0 +1,42 @@
+package com.example.game1;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.ImageButton;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Level3_1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.level3_1);
+        Window w = getWindow();
+        w.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        ImageButton button_start = findViewById(R.id.level2_start);
+        button_start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    Intent intent = new Intent(Level3_1.this, Level3_2.class);
+                    startActivity(intent);
+                    finish();
+                } catch (Exception e) {
+                }
+            }
+        });
+    }
+    @Override
+    public void onBackPressed(){
+        try {
+            Intent intent = new Intent(Level3_1.this, Level1_2.class);
+            startActivity(intent);
+            finish();
+        } catch (Exception e) {
+        }
+    }
+}
